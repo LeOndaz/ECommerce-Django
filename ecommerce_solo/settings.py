9fbc32ba29b23ff3b57170891caeb9b53491b32d
Ahmed I.Elsayed <ahmeddark369@gmail.com>
2019-09-30 06:47:40 +0200

    final

--- ecommerce_solo/settings.py
+++ ecommerce_solo/settings.py
@@ -135,7 +135,9 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'myproject.s3utils.MediaRootS3BotoStorage'
+STATICFILES_STORAGE = 'myproject.s3utils.StaticRootS3BotoStorage'
 
 
 django_heroku.settings(locals())
